65f6cff90d4b6de63e7592b255da8e7daa9b37b5
JamesParrott <80779630+JamesParrott@users.noreply.github.com>
2024-06-28 15:18:07 +0100

    Correct dir in bat file (library name not needed) and try to add platform tag to wheel name

--- rportable_launcher_scripts/Rscript.bat
+++ rportable_launcher_scripts/Rscript.bat
@@ -3,9 +3,8 @@ setlocal
 
 set THIS_FILE_DIR=%~dp0
 set THIS_FILE_NAME=%~n0
-set PKG_NAME=rportable
 set TARGET_DIR=rportable\R-Portable\App\R-Portable\bin\i386
 set EXT=exe
 
 
-start /b /wait cmd /c %THIS_FILE_DIR%\..\Lib\site-packages\%PKG_NAME%\%TARGET_DIR%\%THIS_FILE_NAME%.%EXT% %*
+start /b /wait cmd /c %THIS_FILE_DIR%\..\Lib\site-packages\%TARGET_DIR%\%THIS_FILE_NAME%.%EXT% %*
