eba8f587d845fea1eefea61ec4e2ea6f94da3417
JamesParrott <80779630+JamesParrott@users.noreply.github.com>
2024-07-01 15:22:11 +0100

    Delete references to README.md and LICENSE.md in optional deps. Rename docs wheel.

--- options/docs/build.bat
+++ options/docs/build.bat
@@ -1,3 +1,3 @@
 @echo off
 pdm build --config-setting="--python-tag=py2.py3" --config-setting="--py-limited-api=none" --config-setting="--plat-name=win32.win_amd64" && ^
-move dist\rportable-3.2.3-py2.py3-none-win32_win_amd64.whl dist\rportable-3.2.3-py2.py3-none-win32.win_amd64.whl
+move dist\rportable_docs-3.2.3-py2.py3-none-win32_win_amd64.whl dist\rportable_docs-3.2.3-py2.py3-none-win32.win_amd64.whl
